e0d27eb2b08bd53d739a48ac208c208173d2eb2f
Aayu maurya <aayudevops@gmail.com>
2026-04-03 08:23:31 +0000

    app.js update

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-const http = require('http');
+// Pehle tha:
+res.end('Hello! CI/CD Pipeline Chal Rahi Hai! 🚀\n');
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello! CI/CD Pipeline Chal Rahi Hai! 🚀\n');
-});
-
-server.listen(3000, () => {
-  console.log('Server chal raha hai port 3000 pe!');
-});
+// Ab karo:
+res.end('Maine CI/CD seekh liya! Aayu DevOps Engineer! 🏆\n');
